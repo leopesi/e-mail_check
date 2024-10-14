3067ebfcdd57947eb8e046bc1602a6c089880889
Leonardo Pereira <leopesi@yahoo.com.br>
2024-10-14 00:24:15 -0300

    Update server/config.py

--- project/server/config.py
+++ project/server/config.py
@@ -11,21 +11,23 @@ class BaseConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = False
+    REDIS_URL = 'redis://redis:6379/0'
+    QUEUES = ['default']
 
 
 class DevelopmentConfig(BaseConfig):
-    """Development"""
+    """Development configuration."""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
-    """Testing"""
+    """Testing configuration."""
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
     TESTING = True
 
 
 class ProductionConfig(BaseConfig):
-    """Production"""
+    """Production configuration."""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     WTF_CSRF_ENABLED = True
